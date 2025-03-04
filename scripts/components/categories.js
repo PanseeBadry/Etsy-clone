d44b1bb2757c2d18d645c6edfb4131930f11cd09
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2025-03-04 22:38:23 +0200

    feat: add experimental categories component and styles
    Introduce initial versions of `categories.js` (for a basic categories button) and `categories.css` (to style the dropdown, menus, and sub-category cards). This commit is experimental and under active testing; further refinements and feature additions are expected.

---
+++ scripts/components/categories.js
@@ -0,0 +1,7 @@
+{/* <div class="categories">
+<button class="categories-menu-button hover-effect">
+  <span class="categories-button-data">Categories</span>
+</button>
+</div> */}
+
+let categoriesBtn = document.querySelector('.categories-menu-button');

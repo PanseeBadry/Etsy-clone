7a8b1d683816a4f377aed2ea5485a4163efcdfa3
reem <tarekreem950@gmail.com>
2025-03-09 22:00:25 +0200

    Enhance slider section

--- scripts/components/landingSlider.js
+++ scripts/components/landingSlider.js
@@ -1,11 +1,12 @@
 import Helper from "../utils/helper.js";
 
 export default function renderSlider(products) {
-  let bigDeals = document.getElementById("content");
+  renderScroll();
+  let bigDeals = document.getElementById("carousel");
   let prodArray = products
     .filter((prod) => +prod.discount > 0)
-    .sort((a, b) => b - a);
-  for (let prod of prodArray.slice(0, 12)) {
+    .sort((a, b) => b.discount - a.discount);
+  for (let prod of prodArray.slice(0, 20)) {
     let slide = document.createElement("div");
     slide.setAttribute("data-prod-id", prod.product_id);
     slide.className = "slide";
@@ -42,4 +43,72 @@ export default function renderSlider(products) {
     slide.appendChild(fav);
     bigDeals.appendChild(slide);
   }
+  checkScroll();
+}
+
+function renderScroll() {
+  const carousel = document.getElementById("carousel"),
+    next = document.getElementById("next"),
+    prev = document.getElementById("prev");
+
+  carousel.onscroll = checkScroll;
+  window.addEventListener("resize", checkScroll);
+
+  next.onclick = function () {
+    disableButtons();
+    carousel.scrollBy({
+      left: updateScrollAmount(),
+      behavior: "smooth",
+    });
+    setTimeout(() => {
+      enableButtons();
+    }, 1800);
+  };
+
+  prev.onclick = function () {
+    disableButtons();
+    carousel.scrollBy({
+      left: -updateScrollAmount(),
+      behavior: "smooth",
+    });
+    setTimeout(() => {
+      enableButtons;
+    }, 1800);
+  };
+}
+
+function updateScrollAmount() {
+  const carousel = document.getElementById("carousel");
+  let gap = 16.5;
+  let res;
+  let carouselWidth = carousel.getBoundingClientRect().width;
+  let slides = document.getElementsByClassName("slide");
+  let slideWidth = slides[0].getBoundingClientRect().width;
+
+  if (window.matchMedia("(min-width: 900px)").matches) {
+    res = carouselWidth - slideWidth / 2;
+  } else {
+    res = carouselWidth + gap;
+  }
+  return res;
+}
+
+function checkScroll() {
+  const carousel = document.getElementById("carousel");
+  let maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+  prev.disabled = carousel.scrollLeft <= 0;
+  next.disabled = carousel.scrollLeft >= maxScrollLeft;
+}
+function disableButtons() {
+  const next = document.getElementById("next"),
+    prev = document.getElementById("prev");
+  next.disabled = true;
+  prev.disabled = true;
+}
+
+function enableButtons() {
+  const next = document.getElementById("next"),
+    prev = document.getElementById("prev");
+  next.disabled = false;
+  prev.disabled = false;
 }
